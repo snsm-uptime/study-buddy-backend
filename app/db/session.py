32b71476b37d8fb60e3b0e03237fa4a07c2dbde3
Sebastian Soto M <s.m.sebastian.n@gmail.com>
2025-05-14 13:19:59 -0600

    removed pycache

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,9 @@
-from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
-                                    async_sessionmaker, create_async_engine)
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 from app.config import get_settings
 
