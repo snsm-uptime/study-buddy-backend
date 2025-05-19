369e9ed4f8141021eced2cefbf5dcfe1a574cd48
Sebastian Soto M <s.m.sebastian.n@gmail.com>
2025-05-19 09:21:20 -0600

    removed old logic for file upload

--- app/services/file_service.py
+++ app/services/file_service.py
@@ -1,20 +1,16 @@
-import asyncio
-from typing import Sequence
+from math import e
 from uuid import UUID
 
-from app.db.models.file import File
 from returns.io import IOFailure, IOResult, IOSuccess
-from returns.result import Failure
-from returns.iterables import Fold
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.db.models.file import File
 from app.db.models.file_chunk import FileChunk
 from app.db.repositories.file_chunk_repository import FileChunkRepository
 from app.db.repositories.file_repository import FileRepository
 from app.errors import FileNotFoundError, FormValidationError
 from app.schemas.file import FileCreate, FileRead
-from app.schemas.file_chunk import FileChunkCreate
-from app.services.file_chunk_service import FileChunkService
 from app.utils.text import split_text_into_chunks
 
 
@@ -36,7 +32,7 @@ class FileService:
         content: bytes,
         size_bytes: float,
         author: str = "",
-        source: str = "uploaded",
+        source: str = "file",
     ) -> IOResult[FileRead, FormValidationError]:
         # Parse + chunk
         # chunks = process_file(content, filename)
@@ -45,6 +41,17 @@ class FileService:
 
         # Persist file and chunks in the same transaction
         async with self.session.begin():
+            existing_result = await self.file_repository.get_by_user_and_title_size(
+                user_id=user_id,
+                title=filename,
+                size_bytes=size_bytes,
+            )
+            if not isinstance(existing_result, IOFailure):
+                return IOFailure(
+                    FormValidationError(
+                        field="file", message="This file has already been uploaded."
+                    )
+                )
             file_create_response = await self.file_repository.create(
                 user_id=user_id,
                 title=filename,
@@ -75,84 +82,21 @@ class FileService:
                                     message="Failed to persist file chunks",
                                 )
                             )
+                case IOFailure(exception):
+                    err = exception.failure()
+                    failure: FormValidationError
+                    if isinstance(err, IntegrityError):
+                        failure = FormValidationError(
+                            field="file",
+                            message=f"[FAIL:FILE] {err.params} {err.args[0]}",
+                        )
+                    return IOFailure(failure)
                 case _:
                     return IOFailure(
                         FormValidationError(
-                            field="file", message="Failed to upload file"
+                            field="file", message=f"Failed to upload file"
                         )
                     )
-
-    # async def process_and_store_file(
-    #     self,
-    #     *,
-    #     filename: str,
-    #     size: int,
-    #     content: bytes,
-    #     user_id: UUID,
-    #     author: str,
-    #     source: str,
-    # ) -> IOResult[FileRead, FormValidationError]:
-    #     try:
-
-    #         file_create = FileCreate(
-    #             title=filename,
-    #             author=author,
-    #             size_bytes=size,
-    #             source=source,
-    #             user_id=user_id,
-    #         )
-
-    #         # Upload the file metadata
-    #         file_result = await self.upload_file(file_create)
-
-    #         match file_result:
-    #             case IOSuccess(file_data):
-    #                 file = file_data.unwrap()
-    #                 create_batch = []
-    #                 for index, chunk in enumerate(chunks):
-    #                     fc = FileChunkCreate(
-    #                         file_id=file.id,
-    #                         chunk_index=index,
-    #                         section=None,
-    #                         text=chunk,
-    #                     )
-    #                     # TODO: Each chunk analyze if there's a section it can be grouped into.
-    #                     create_batch.append(self.file_chunk_service.create(fc))
-    #                 await asyncio.gather(*create_batch)
-    #                 return IOSuccess.from_result(file_data)
-    #             case _:
-    #                 return file_result
-    #     except Exception as e:
-    #         return IOFailure(FormValidationError(field="file", message=str(e)))
-
-    async def upload_file(
-        self, file_data: FileCreate
-    ) -> IOResult[FileRead, FormValidationError]:
-        # Check if the file already exists for the user
-        existing_result = await self.file_repository.get_by_user_and_title_size(
-            user_id=file_data.user_id,
-            title=file_data.title,
-            size_bytes=file_data.size_bytes,
-        )
-
-        match existing_result:
-            case IOSuccess(_):
-                return IOFailure(
-                    FormValidationError(
-                        field="file", message="This file has already been uploaded."
-                    )
-                )
-            case _:
-                result = await self.file_repository.create(**file_data.model_dump())
-                match result:
-                    case IOSuccess(f):
-                        return IOSuccess(FileRead.model_validate(f.unwrap()))
-                    case _:
-                        return IOFailure(
-                            FormValidationError(
-                                field="file", message="Failed to upload file"
-                            )
-                        )
 
     async def get_file_by_id(
         self, file_id: UUID
